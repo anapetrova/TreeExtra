3f83ec158593b88d17a55ccff999bc0474700b0e
Daria Sorokina <fisharik@gmail.com>
2017-11-22 15:10:30 -0800

    changed bagging recommendations to increase proportionally

--- BaggedTrees/bt_train.cpp
+++ BaggedTrees/bt_train.cpp
@@ -270,7 +270,7 @@ int main(int argc, char* argv[])
 	//analyze whether more bagging should be recommended based on the curve in the best point
 	if(moreBag(rmsV))
 	{
-		int recBagN = ti.bagN + 100;
+		int recBagN = roundInt(round(ti.bagN * 1.5));
 		clog << "\nRecommendation: a greater number of bagging iterations might produce a better model.\n"
 			<< "Suggested action: bt_train -b " << recBagN << "\n";
 	}
